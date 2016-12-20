36b30c80f23724635c3f7cf055f79333b69fc713
Tanguy SCHOLTES <soultaker720@hotmail.com>
2016-12-20 09:31:38 +0100

    first commit - laravel initialized and navigation view

---
+++ resources/views/_navigation/navigation.blade.php
@@ -0,0 +1,15 @@
+<nav>
+    <h2>Navigation principale</h2>
+
+    <a href="/">Accueil</a>
+    <a href="/universe">Univers</a>
+    <a href="/rules">Règles de base</a>
+    <a href="/races">Races</a>
+    <a href="/classes">Classes</a>
+    <a href="/abilities">Caractéristiques</a>
+    <a href="/skills">Compétences</a>
+    <a href="/feats">Dons</a>
+    <a href="/powers">Pouvoirs</a>
+    <a href="/items">Objets</a>
+    <a href="/alignement">Alignements</a>
+</nav>
